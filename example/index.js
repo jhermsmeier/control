10113130dbd4dda366cd3a0f6791044c527abad3
Jonas Hermsmeier <jhermsmeier@gmail.com>
2020-09-28 20:53:00 +0200

    package: Use strict, fix running under Node v4
    Close #1

--- example/index.js
+++ example/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var assert = require( 'assert' )
 var { context, setup, teardown, test } = require( '..' )
 
--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var { context, test } = require( '..' )
 
 context( 'Control', function() {})
