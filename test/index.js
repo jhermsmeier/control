2c992b55400d6eddd14c65a6c1ffe4af393816b6
Jonas Hermsmeier <jhermsmeier@gmail.com>
2020-09-28 21:26:55 +0200

    control: Fix running before/after tasks
    Close #3

--- test/index.js
+++ test/index.js
@@ -1,5 +1,29 @@
 'use strict'
 
-var { context, test } = require( '..' )
+var { context, test, before, after, setup, teardown } = require( '..' )
 
-context( 'Control', function() {})
+context( 'Control', function() {
+
+  console.log( 'context' )
+
+  setup( 'setup', function() {
+    console.log( 'setup' )
+  })
+
+  teardown( 'teardown', function() {
+    console.log( 'teardown' )
+  })
+
+  before( 'before', function() {
+    console.log( 'before' )
+  })
+
+  test( 'test', function() {
+    console.log( 'test' )
+  })
+
+  after( 'after', function() {
+    console.log( 'after' )
+  })
+
+})
